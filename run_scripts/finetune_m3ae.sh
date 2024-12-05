654a873c26a3913ccf4d3780e68b5a221c8619d7
mycoalchen <chen.michael.1116@gmail.com>
2024-12-05 23:56:31 +0000

    set up tests for different mm encoder inputs

--- run_scripts/finetune_m3ae.sh
+++ run_scripts/finetune_m3ae.sh
@@ -1,4 +1,4 @@
-num_gpus=1
+num_gpus=5
 per_gpu_batchsize=8
 
 
@@ -21,8 +21,11 @@ python main_t5_m3ae.py with data_root=data/finetune_arrows/ \
  t5_max_length=12 \
  learning_rate=0.00001 \
  batch_size=64 \
- unfreeze_num_encoder_layers=3 \
- unfreeze_num_decoder_layers=5 \
+ mm_encoder_inputs_include_cls_feats = True \
+ mm_encoder_inputs_include_imagetext_feats = False \
+ mm_encoder_inputs_mm_feats_width = 0 \
+ unfreeze_num_encoder_layers=4 \
+ unfreeze_num_decoder_layers=4 \
  num_gpus=${num_gpus} num_nodes=1 \
  task_finetune_vqa_vqa_rad \
  per_gpu_batchsize=${per_gpu_batchsize} \
@@ -34,11 +37,14 @@ python main_t5_m3ae.py with data_root=data/finetune_arrows/ \
 python main_t5_m3ae.py with data_root=data/finetune_arrows/ \
  num_workers=0 \
  max_epoch=70 \
- t5_max_length=50 \
+ t5_max_length=12 \
  learning_rate=0.00001 \
  batch_size=64 \
- unfreeze_num_encoder_layers=3 \
- unfreeze_num_decoder_layers=5 \
+ mm_encoder_inputs_include_cls_feats = True \
+ mm_encoder_inputs_include_imagetext_feats = True \
+ mm_encoder_inputs_mm_feats_width = 10 \
+ unfreeze_num_encoder_layers=4 \
+ unfreeze_num_decoder_layers=4 \
  num_gpus=${num_gpus} num_nodes=1 \
  task_finetune_vqa_vqa_rad \
  per_gpu_batchsize=${per_gpu_batchsize} \
@@ -51,10 +57,14 @@ python main_t5_m3ae.py with data_root=data/finetune_arrows/ \
 python main_t5_m3ae.py with data_root=data/finetune_arrows/ \
  num_workers=0 \
  max_epoch=70 \
+ t5_max_length=12 \
  learning_rate=0.00001 \
  batch_size=64 \
- unfreeze_num_encoder_layers=5 \
- unfreeze_num_decoder_layers=3 \
+ mm_encoder_inputs_include_cls_feats = False \
+ mm_encoder_inputs_include_imagetext_feats = True \
+ mm_encoder_inputs_mm_feats_width = 10 \
+ unfreeze_num_encoder_layers=4 \
+ unfreeze_num_decoder_layers=4 \
  num_gpus=${num_gpus} num_nodes=1 \
  task_finetune_vqa_vqa_rad \
  per_gpu_batchsize=${per_gpu_batchsize} \
@@ -67,10 +77,14 @@ python main_t5_m3ae.py with data_root=data/finetune_arrows/ \
 python main_t5_m3ae.py with data_root=data/finetune_arrows/ \
  num_workers=0 \
  max_epoch=70 \
+ t5_max_length=12 \
  learning_rate=0.00001 \
  batch_size=64 \
- unfreeze_num_encoder_layers=5 \
- unfreeze_num_decoder_layers=5 \
+ mm_encoder_inputs_include_cls_feats = False \
+ mm_encoder_inputs_include_imagetext_feats = True \
+ mm_encoder_inputs_mm_feats_width = 20 \
+ unfreeze_num_encoder_layers=4 \
+ unfreeze_num_decoder_layers=4 \
  num_gpus=${num_gpus} num_nodes=1 \
  task_finetune_vqa_vqa_rad \
  per_gpu_batchsize=${per_gpu_batchsize} \
